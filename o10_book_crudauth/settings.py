87412bf55a296bc63388403b04ce8390f7cb5437
Doyin <doyinelugbadebo@gmail.com>
2025-01-06 06:18:36 +0100

    updated .env

--- o10_book_crudauth/settings.py
+++ o10_book_crudauth/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = "o10_book_crudauth.wsgi.application"
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgres://ngvmhgra:3dP2vWmYpZcA2-kf3iEOqu6UAhu7ij29@kandula.db.elephantsql.com:5432/ngvmhgra',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )
 }
